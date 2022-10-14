deb18683d28276e65ea222aa7886e6e2282021ea
szdygal <sdzygal@gmail.com>
2022-10-14 15:23:15 -0600

    Added Testimonial section

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ modalCloses.forEach((modalClose) => {
   })
 })
 
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
     navigation: {
@@ -77,4 +77,22 @@ let swiper = new Swiper(".portfolio__container", {
       clickable: true,
     },
   });
-      
+
+
+  let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        568:{
+            slidesPerView: 2,
+        }
+    }
+    
+  });
